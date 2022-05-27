9fc9a0313243cf59305ba93f328098318d71d61a
Mustafa1845 <mustafa@irenictech.com>
2022-05-27 22:40:24 +0000

    another fix

--- scripts/build_and_publish_package.sh
+++ scripts/build_and_publish_package.sh
@@ -9,7 +9,7 @@ START_TIME=$4
 PRIVATE_ECR=$5
 REGION=$6
 
-DOCKER_BUILDKIT=1 docker build -t $LABEL-$PACKAGE -f dockerfiles/$PACKAGE/Dockerfile-$PACKAGE .
+DOCKER_BUILDKIT=1 docker build -t $LABEL-$PACKAGE -f ./dockerfiles/$PACKAGE/Dockerfile-$PACKAGE \
   --build-arg MYSQL_HOST=$MYSQL_HOST \
   --build-arg MYSQL_PORT=$MYSQL_PORT \
   --build-arg MYSQL_PASSWORD=$MYSQL_PASSWORD \
