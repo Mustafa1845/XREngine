15b3325ed99e416de995e0119a7eaebfa88fb6f1
Mustafa1845 <mustafa@irenictech.com>
2022-06-09 18:23:06 +0500

    Added prod-builder

--- scripts/setup_helm.sh
+++ scripts/setup_helm.sh
@@ -20,7 +20,7 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-helm repo add xrengine/xrengine-builder --name prod --version 4.0.1
+helm repo add prod-builder xrengine/xrengine-builder --name prod --version 4.0.1
 
 
 helm repo update
