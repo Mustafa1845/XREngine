4c150f8769886a815608dd34fa17f2cc580d2e3f
Mustafa1845 <mustafa@irenictech.com>
2022-06-09 18:10:20 +0500

    add version specific

--- scripts/setup_helm_builder.sh
+++ scripts/setup_helm_builder.sh
@@ -12,7 +12,7 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-helm repo add stable https://charts.helm.sh/stable
+helm repo add xrengine/xrengine-builder --name prod-builder --version 4.0.1
 
 helm repo update
 
