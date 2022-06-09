539d289fa041a32dab96e6ee7e504a4aabf48b59
Mustafa1845 <mustafa@irenictech.com>
2022-06-09 17:41:36 +0500

    Added helm repo add stable https://charts.helm.sh/stable

--- scripts/setup_helm.sh
+++ scripts/setup_helm.sh
@@ -20,7 +20,8 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm repo add stable https://charts.helm.sh/stable
+
 
 helm repo update
 
