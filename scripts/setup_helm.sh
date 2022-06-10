d54547d482d5ebeb11e405acb693e24752e05ef1
Mustafa1845 <mustafa@irenictech.com>
2022-06-10 14:37:18 +0500

    added --v

--- scripts/setup_helm.sh
+++ scripts/setup_helm.sh
@@ -22,6 +22,6 @@ chmod 700 get_helm.sh
 
 helm repo add xrengine https://helm.xrengine.io
 
-helm repo update xrengine -version 4.0.1 https://helm.xrengine.io
+helm repo update xrengine --version 4.0.1 https://helm.xrengine.io
 
 set +x
