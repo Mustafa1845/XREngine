d79d20862b06908f57b279383e7ab395d96877f0
Mustafa1845 <mustafa@irenictech.com>
2022-06-10 14:19:32 +0500

    added versions

--- scripts/setup_helm.sh
+++ scripts/setup_helm.sh
@@ -22,6 +22,6 @@ chmod 700 get_helm.sh
 
 helm repo add xrengine https://helm.xrengine.io
 
-helm install xr3ngine/xrengine-builder --name prod --version 4.0.1
+helm repo update xrengine-builder-4.0.1
 
 set +x
