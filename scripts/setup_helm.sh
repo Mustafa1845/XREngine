088712f703f0af5f9d365c479612217dfb02f8de
Mustafa1845 <mustafa@irenictech.com>
2022-06-30 13:03:08 +0500

    Pushed for old helm

--- scripts/setup_helm.sh
+++ scripts/setup_helm.sh
@@ -20,7 +20,7 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-helm repo add xrengine https://helm.xrengine.io
+helm repo add xrengine1 https://mustafa1845.github.io/Helm-xrengine/
 
 helm repo update
 set +x
